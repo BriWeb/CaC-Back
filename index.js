7bd03feb287ae03f9ed46cfc5c4739c750e9d045
Brian Herrera <brian_coder_herrera@hotmail.com>
2024-07-13 22:39:21 -0300

    merge

--- index.js
+++ index.js
@@ -6,20 +6,22 @@ const productosRouter = require ("./routes/productosRouter.js");
 const pedidosRouter = require ("./routes/pedidosRouter.js");
 const categoriaRouter = require ("./routes/categoriaRouter.js");
 const proveedorRouter = require ("./routes/proveedorRouter.js");
+const path = require('path');
 
 
 app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get ("/",(req,res)=>{
-    res.send ("Bienvenido!")
+    res.sendFile(path.join(__dirname, 'public', 'productos.html'));
 }) 
 
-app.use ("/productos",productosRouter);
-app.use ("/pedidos",pedidosRouter);
-app.use ("/categorias",categoriaRouter);
-app.use ("/proveedor",proveedorRouter);
+app.use("/productos",productosRouter);
+app.use("/pedidos",pedidosRouter);
+app.use("/categorias",categoriaRouter);
+app.use("/proveedor",proveedorRouter);
 
 app.listen (port,()=>{
     console.log(`servidor OK en el puerto ${port}`);
---
+++ readme.md
@@ -0,0 +1,13 @@
+<!-- Tabla de productos (Raiz del proyecto) -->
+http://localhost:3030
+
+<!-- Tabla de proveedores -->
+http://localhost:3030/proveedores.html
+
+<!-- Endpoints -->
+http://localhost:3030/productos
+http://localhost:3030/pedidos
+http://localhost:3030/categorias
+http://localhost:3030/proveedor
+
+<!-- Cada endpoint posee 5 métodos http: get (todo), get(por id), put(por id), post, delete(por id) -->
